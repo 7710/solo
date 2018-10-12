e59a8a3b7d4bdb598fb3b21c1a84ef22f19366de
7710 <7710net@gmail.com>
2018-10-12 23:13:12 +0800

    批量下载4chan图片

--- 4chan.py
+++ 4chan.py
@@ -5,17 +5,67 @@ import sys
 import time
 import urllib
 from lxml import etree
+from requests.packages import urllib3
+
+""" 批量下载4chan页面中的图片
+"""
 
 def timeft(t, fs="%Y-%m-%d %H:%M:%S"):
     return time.strftime(fs, t)
 
+
+def geturls():
+    """ 获取配置文件内的url列表
+        配置文件与脚本同名，无后缀
+    """
+    a = []
+    s = os.path.basename(__file__).split('.')[0]
+    if not os.path.exists(s):
+        return a
+    with open(s) as f:
+        l = f.readline()
+        while l:
+            a.append(l.strip())
+            l = f.readline()
+    return a
+
+
+def writeimg(img, path):
+    """ 保存图片
+    """
+    conn = urllib.request.urlopen("http:"+img, timeout=30)
+    f = open(path, 'wb')
+    f.write(conn.read())
+    f.close()
+
+
+def getimg(link, path):
+    """ 获取图片url
+    """
+    img = link.attrib.get("href")
+    filename = img.split('/')[-1]
+    filepath = "%s/%s" % (path, filename)
+    if os.path.isfile(filepath):
+        return
+    print(filename)
+    n = 0
+    while n < 3:
+        try:
+            writeimg(img, filepath)
+            time.sleep(3)
+        except:
+            n += 1
+            print("retry %d" % n)
+        else:
+            break
+
+
 def main():
-    _PATH = "/home/sunq/Pictures/4chan"
-    urls = ["http://boards.4chan.org/s/thread/18447811"
-                #, "http://boards.4chan.org/s/thread/18439093"
-                #, "http://boards.4chan.org/s/thread/18441231"
-            ]
+    # 保存路径
+    _PATH = "/home/public/Pictures/4chan"
+    urls = geturls()
     for url in urls:
+        print(url)
         ua = url.split('/')
         path = "%s/%s" % (_PATH, ua[-3])
         if not os.path.exists(path):
@@ -23,26 +73,17 @@ def main():
         path = "%s/%s" % (path, ua[-1])
         if not os.path.exists(path):
             os.makedirs(path)
-        requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         r = requests.get(url=url,
                          allow_redirects=False,
                          verify=False,
                          timeout=30)
         content = r.content.decode('ISO-8859-1')
-        #html = etree.HTML(content.decode('utf-8'))
         html = etree.HTML(content)
         links = html.xpath("//a[@class='fileThumb']")
         for link in links:
-            img = link.attrib.get("href")
-            filename = img.split('/')[-1]
-            print("%s-%s" % (ua[-3], filename))
-            filepath = "%s/%s" % (path, filename)
-            if not os.path.isfile(filepath):
-                conn = urllib.request.urlopen("http:"+img, timeout=30)
-                f = open(filepath, 'wb')
-                f.write(conn.read())
-                f.close()
-                time.sleep(3)
+            getimg(link, path)
+
 
 if __name__ == '__main__':
     main()
